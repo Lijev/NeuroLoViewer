91d165b12dbdf9c2ec892fc76100e83e84b3857a
NormisSrediGeniev <ninjavmedia@gmail.com>
2025-03-01 22:59:04 +0100

    Updated Instrument
    The user can now insert Video Length without having to restart the module each time

--- ChronoDelilka.py
+++ ChronoDelilka.py
@@ -1,17 +1,23 @@
 def split_video():
-    try:
-        duration = float(input("Enter the video timing (in minutes with seconds after the decimal point): "))
-        duration_seconds = duration * 60  
-        segment_length = duration_seconds / 10  
+    while True:
+        try:
+            duration = input("\nEnter the timing of the video (in minutes with seconds after the decimal point, or 'exit' to exit): ")
+            if duration.lower() == 'exit':
+                print(" Quitting...")
+                break  
 
-        print("\nDividing the video into 10 equal chunks:")
-        for i in range(1, 11):
-            segment_time = segment_length * i
-            minutes = int(segment_time // 60)
-            seconds = round(segment_time % 60, 2)
-            print(f"chunk {i}: {minutes} min {seconds} sec")
+            duration = float(duration)  
+            duration_seconds = duration * 60  
+            segment_length = duration_seconds / 10  
 
-    except ValueError:
-        print("Error: input correct valueble (exmp, 5.30 for 5 minutes 30 seconds).")
+            print("\nSplitting video in 10 chunks")
+            for i in range(1, 11):
+                segment_time = segment_length * i
+                minutes = int(segment_time // 60)
+                seconds = round(segment_time % 60, 2)
+                print(f"chunk {i}: {minutes} min {seconds} sec")
+
+        except ValueError:
+            print("Error: Enter a valid number (e.g. 5.30 for 5 minutes 30 seconds) or 'exit' to exit.")
 
 split_video()
